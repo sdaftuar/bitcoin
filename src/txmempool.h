d22b852fd2d6526cbdaa2ad48358c86792361ff6
Alex Morcos <morcos@chaincode.com>
2015-07-22 10:57:03 -0500

    Refactor STTS to be usable for surplus trimming as well

--- src/txmempool.h
+++ src/txmempool.h
@@ -319,6 +319,8 @@ public:
      *  - Removing said list will reduce the DynamicMemoryUsage after adding toadd, below sizelimit.
      */
     bool StageTrimToSize(size_t sizelimit, const CTxMemPoolEntry& toadd, CAmount nFeesReserved, std::set<uint256>& stage, CAmount& nFeesRemoved);
+    bool TrimMempool(size_t sizeToTrim, std::set<uint256> &protect, CAmount nFeesReserved, size_t sizeToUse, CAmount feeToUse,
+		     std::set<uint256>& stage, CAmount &nfeesRemoved);
 
     /** Expire all transaction (and their dependencies) in the mempool older than time. Return the number of removed transactions. */
     int Expire(int64_t time);
