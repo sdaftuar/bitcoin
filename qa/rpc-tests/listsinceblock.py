0a10605b782c1b7997833e1a3644cb02a6c437da
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-02-26 18:26:34 -0800

    [qa] listsinceblock.py updated to include a 3-chain version of testreorg.
    Includes modifications by sdaftuar@chaincode.com

--- qa/rpc-tests/listsinceblock.py
+++ qa/rpc-tests/listsinceblock.py
@@ -14,7 +14,7 @@ class ListSinceBlockTest (BitcoinTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 4
 
-    def run_test (self):
+    def test_reorg(self):
         '''
         `listsinceblock` did not behave correctly when handed a block that was
         no longer in the main chain:
@@ -76,6 +76,60 @@ class ListSinceBlockTest (BitcoinTestFramework):
                 found = True
                 break
         assert_equal(found, True)
+
+    def test_reorg3(self):
+        '''
+        Same as above, except there are now three conflicting chains.
+        Use invalidateblock() on node1 to allow it to mine different
+        chains from a common fork point.
+        '''
+
+        assert_equal(self.is_network_split, False)
+
+        # send to nodes[0] from nodes[2]
+        senttx = self.nodes[2].sendtoaddress(self.nodes[0].getnewaddress(), 1)
+
+        self.sync_all() # Ensure the tx has propagated to all nodes
+
+        generated_tips = [] # track generated tips to sanity check the test
+
+        # generate 1 chain, and sync
+        blockhashes = self.nodes[1].generate(6)
+        lastblockhash = blockhashes[-1]
+        generated_tips.append(lastblockhash)
+        print('lastblockhash=%s' % (lastblockhash))
+        self.sync_all()
+
+        # generate an alternate chain that is longer
+        self.nodes[1].invalidateblock(blockhashes[0])
+        blockhashes = self.nodes[1].generate(7)
+        generated_tips.append(blockhashes[-1])
+        self.sync_all()
+
+        # generate one more alternate chain that is longer still
+        self.nodes[1].invalidateblock(blockhashes[0])
+        blockhashes = self.nodes[1].generate(8)
+        generated_tips.append(blockhashes[-1])
+        self.sync_all()
+
+        # node0 should have all 3 chains (or else this test is broken!)
+        tips = [x['hash'] for x in self.nodes[0].getchaintips()]
+        assert(len(tips) >= 3)
+        for t in generated_tips:
+            assert(t in tips)
+
+        # listsinceblock(lastblockhash) should now include tx, as seen from nodes[0]
+        lsbres = self.nodes[0].listsinceblock(lastblockhash)
+        found = False
+        for tx in lsbres['transactions']:
+            if tx['txid'] == senttx:
+                found = True
+                break
+        assert_equal(found, True)
+
+    def run_test(self):
+        self.test_reorg()
+        self.test_reorg3()
 
 if __name__ == '__main__':
     ListSinceBlockTest().main()
