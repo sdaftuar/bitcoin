6906ba2b9c7cce579cfc81b944d6341b4a7428fe
John Newbery <john@johnnewbery.com>
2017-03-08 17:46:18 -0500

    Move src/test/bitcoin-util-test.py to test/util/bitcoin-util-test.py

--- test/util/bitcoin-util-test.py
+++ test/util/bitcoin-util-test.py
@@ -14,9 +14,9 @@ help_text="""Test framework for bitcoin utils.
 
 Runs automatically during `make check`.
 
-Can also be run manually from the src directory by specifying the source directory:
+Can also be run manually from the source directory:
 
-test/bitcoin-util-test.py --srcdir='srcdir' [--verbose]
+test/util/bitcoin-util-test.py --srcdir='.' [--verbose]
 """
 
 if __name__ == '__main__':
@@ -42,4 +42,4 @@ if __name__ == '__main__':
     # Add the format/level to the logger
     logging.basicConfig(format = formatter, level=level)
 
-    bctest.bctester(srcdir + "/test/data", "bitcoin-util-test.json", buildenv)
+    bctest.bctester(srcdir + "/../test/util/data", "bitcoin-util-test.json", buildenv)
