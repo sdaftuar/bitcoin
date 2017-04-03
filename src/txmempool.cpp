6676c6ab5386b3db8902750111d5da5925def9fe
Suhas Daftuar <sdaftuar@gmail.com>
2017-04-03 16:31:51 -0400

    PrioritiseTransaction updates the mempool tx counter

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -912,6 +912,7 @@ void CTxMemPool::PrioritiseTransaction(const uint256& hash, const CAmount& nFeeD
             BOOST_FOREACH(txiter ancestorIt, setAncestors) {
                 mapTx.modify(ancestorIt, update_descendant_state(0, nFeeDelta, 0));
             }
+            ++nTransactionsUpdated;
         }
     }
     LogPrintf("PrioritiseTransaction: %s feerate += %s\n", hash.ToString(), FormatMoney(nFeeDelta));
