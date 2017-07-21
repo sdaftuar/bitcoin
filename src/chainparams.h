a5c9be2dfd0b8eebee74d4849e114d052d5f9b5b
Alex Morcos <morcos@chaincode.com>
2017-07-21 15:41:48 -0400

    Segsignal compatible UASF

--- src/chainparams.h
+++ src/chainparams.h
@@ -119,4 +119,9 @@ void SelectParams(const std::string& chain);
  */
 void UpdateRegtestBIP9Parameters(Consensus::DeploymentPos d, int64_t nStartTime, int64_t nTimeout);
 
+/**
+ * Allows modifying the BIP91 regtest height.
+ */
+void UpdateRegtestBIP91Height(int height);
+
 #endif // BITCOIN_CHAINPARAMS_H
