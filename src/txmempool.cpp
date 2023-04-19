60d2bbece60049f4a2bb5ecdacd8b46457f357dc
Suhas Daftuar <sdaftuar@chaincode.com>
2023-04-19 17:12:04 -0400

    Expose cluster information via rpc

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -1533,6 +1533,7 @@ void Cluster::AddTransaction(const CTxMemPoolEntry& entry, bool sort)
     m_chunks.back().txs.emplace_back(entry);
     entry.m_cluster = this;
     ++m_tx_count;
+    m_tx_size += entry.GetTxSize();
     if (sort) Sort();
     return;
 }
@@ -1548,12 +1549,14 @@ void Cluster::RemoveTransaction(const CTxMemPoolEntry& entry)
     // able to do multiple removals in a row and then clean up the sort, we
     // can't clean up empty chunks here.
     --m_tx_count;
+    m_tx_size -= entry.GetTxSize();
     return;
 }
 
 void Cluster::RechunkFromLinearization(std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef>& txs, bool reassign_locations)
 {
     m_chunks.clear();
+    m_tx_size = 0;
 
     for (auto txentry : txs) {
         m_chunks.emplace_back(txentry.get().GetModifiedFee(), txentry.get().GetTxSize());
@@ -1576,6 +1579,7 @@ void Cluster::RechunkFromLinearization(std::vector<CTxMemPoolEntry::CTxMemPoolEn
                 break;
             }
         }
+        m_tx_size += txentry.get().GetTxSize();
     }
 
     if (reassign_locations) {
