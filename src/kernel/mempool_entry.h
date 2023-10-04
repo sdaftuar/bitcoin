67767968693b21724fef8011a4785f2465c7594b
Suhas Daftuar <sdaftuar@chaincode.com>
2023-10-04 09:48:04 -0400

    Remove unused members from CTxMemPoolEntry

--- src/kernel/mempool_entry.h
+++ src/kernel/mempool_entry.h
@@ -87,21 +87,6 @@ private:
     CAmount m_modified_fee;         //!< Used for determining the priority of the transaction for mining in a block
     LockPoints lockPoints;          //!< Track the height and time at which tx was final
 
-    // Information about descendants of this transaction that are in the
-    // mempool; if we remove this transaction we must remove all of these
-    // descendants as well.
-    int64_t m_count_with_descendants{1}; //!< number of descendant transactions
-    // Using int64_t instead of int32_t to avoid signed integer overflow issues.
-    int64_t nSizeWithDescendants;      //!< ... and size
-    CAmount nModFeesWithDescendants;   //!< ... and total fees (all including us)
-
-    // Analogous statistics for ancestor transactions
-    int64_t m_count_with_ancestors{1};
-    // Using int64_t instead of int32_t to avoid signed integer overflow issues.
-    int64_t nSizeWithAncestors;
-    CAmount nModFeesWithAncestors;
-    int64_t nSigOpCostWithAncestors;
-
 public:
     CTxMemPoolEntry(const CTransactionRef& tx, CAmount fee,
                     int64_t time, unsigned int entry_height, uint64_t entry_sequence,
@@ -117,12 +102,7 @@ public:
           spendsCoinbase{spends_coinbase},
           sigOpCost{sigops_cost},
           m_modified_fee{nFee},
-          lockPoints{lp},
-          nSizeWithDescendants{GetTxSize()},
-          nModFeesWithDescendants{nFee},
-          nSizeWithAncestors{GetTxSize()},
-          nModFeesWithAncestors{nFee},
-          nSigOpCostWithAncestors{sigOpCost} {}
+          lockPoints{lp} {}
 
     const CTransaction& GetTx() const { return *this->tx; }
     CTransactionRef GetSharedTx() const { return this->tx; }
@@ -147,8 +127,6 @@ public:
     // Updates the modified fees with descendants/ancestors.
     void UpdateModifiedFee(CAmount fee_diff)
     {
-        nModFeesWithDescendants = SaturatingAdd(nModFeesWithDescendants, fee_diff);
-        nModFeesWithAncestors = SaturatingAdd(nModFeesWithAncestors, fee_diff);
         m_modified_fee = SaturatingAdd(m_modified_fee, fee_diff);
     }
 
