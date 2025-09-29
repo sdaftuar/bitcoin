5de0b3f7a0d0f5e7c87ceaf87b5c0984bc2a4e44
Pieter Wuille <pieter@wuille.net>
2025-09-29 18:06:19 -0400

    WIP fancyfrontier

--- src/util/frontier.h
+++ src/util/frontier.h
@@ -73,6 +73,58 @@ public:
     uint64_t Updates() const noexcept { return m_updates; }
 };
 
+template<typename ValueType>
+class FancyFrontier
+{
+    std::vector<ValueType> m_data;
+    uint64_t m_updates{0};
+
+public:
+    FancyFrontier(size_t reserve, ValueType init) noexcept
+    {
+        m_data.assign(reserve + 1, init);
+    }
+
+    bool Set(size_t key, ValueType value) noexcept
+    {
+        bool ret = false;
+        while (value > m_data[key]) {
+            ret = true;
+            m_data[key] = value;
+            key += (key & (~key + 1));
+            if (key >= m_data.size()) break;
+        }
+    }
+
+    bool Test(size_t key, ValueType value) const noexcept
+    {
+        while (true) {
+            if (m_data[key] >= value) return true;
+            if (key == 0) break;
+            key -= (key & (~key + 1));
+        }
+    }
+
+    std::pair<size_t, ValueType> Last() noexcept
+    {
+        while (m_data.size() > 1 && *m_data.rbegin() == *(m_data.rbegin() + 1)) {
+            m_data.pop_back();
+        }
+        return {m_data.size() - 1, m_data.back()};
+    }
+
+    bool PreviousTick(std::pair<size_t, ValueType>& cur) const noexcept
+    {
+        if (cur.first == 0) return false;
+        --cur.first;
+        cur.second = m_data[cur.first];
+        while (cur.first > 0 && cur.second == m_data[cur.first - 1]) --cur.first;
+        return true;
+    }
+
+    uint64_t Updates() const noexcept { return m_updates; }
+};
+
 } // namespace
 
 #endif // BITCOIN_UTIL_GOLOMBRICE_H
